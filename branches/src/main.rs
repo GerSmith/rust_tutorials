4107803a08a4abb8d808713da95a943d898141df
GerSmith <simakovserg@gmail.com>
2022-02-03 10:40:24 +0500

    Условия

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,3 +1,33 @@
+/*
+    Управление потоком
+*/
+
 fn main() {
-    println!("Hello, world!");
+    let number = 5;
+
+    if number > 3 {
+        println!("Condition was true!")
+    } else {
+        println!("Condition was false!")
+    }
+
+    if number != 0 {
+        println!("number was something other than zero");
+    }
+
+    let number = 6; // shadowed
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+
+    let condition = true;
+    let num = if condition { 5 } else { 3 };
+    println!("The value of number is: {}", num);
 }
