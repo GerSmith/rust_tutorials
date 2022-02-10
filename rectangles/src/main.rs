4b657479cb4e0b00ff2ab10f45064032f863f688
GerSmith <simakovserg@gmail.com>
2022-02-10 16:19:34 +0500

    debug derive

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -2,16 +2,54 @@
     Вычисление площади прямоугольника
 */
 
-fn main() {
-    let width1: u32 = 30;
-    let height1: u32 = 30;
-
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(width1, height1)
-    );
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
+fn main() {
+    /*
+    let width1: u32 = 30;
+    let height1: u32 = 30;
+    */
+
+    /*
+    let rect1: (u32, u32) = (30, 50);
+    */
+
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!("rect1 is {:?}", rect1);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(&rect1)
+    );
+
+    let scale = 2;
+    let rect2 = Rectangle {
+        width: 10,
+        height: dbg!(scale * 33),
+    };
+
+    dbg!(&rect2);
+}
+
+/*
 fn area(width: u32, height: u32) -> u32 {
     width * height
+}
+*/
+
+/*
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
+*/
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
