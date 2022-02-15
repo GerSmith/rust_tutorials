fde0f3d186ff9d55ac4f44b0c1cfaf01ab05d564
GerSmith <simakovserg@gmail.com>
2022-02-15 09:43:09 +0500

    fix impl for struct

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -7,49 +7,20 @@ struct Rectangle {
     width: u32,
     height: u32,
 }
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
 
 fn main() {
-    /*
-    let width1: u32 = 30;
-    let height1: u32 = 30;
-    */
-
-    /*
-    let rect1: (u32, u32) = (30, 50);
-    */
-
     let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
 
-    println!("rect1 is {:?}", rect1);
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect1)
+        rect1.area()
     );
-
-    let scale = 2;
-    let rect2 = Rectangle {
-        width: 10,
-        height: dbg!(scale * 33),
-    };
-
-    dbg!(&rect2);
-}
-
-/*
-fn area(width: u32, height: u32) -> u32 {
-    width * height
-}
-*/
-
-/*
-fn area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
-}
-*/
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
 }
